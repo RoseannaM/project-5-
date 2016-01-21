9dfe041a51f50c09db881b8c45318b2742487562
Roseanna McFarlane <play.with.fashion@gmail.com>
2016-01-21 23:43:07 +1100

    adding markers to map

--- js/app.js
+++ js/app.js
@@ -1,123 +1,116 @@
-var map;    // declares a global map variable
+var locationData = [
+    {
+        name: 'Royal Botanic Gardens',
+        latLng: {lat: -33.868691, lng: 151.217736},
+        description: '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+        '<div id="bodyContent">'+
+        '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+        'sandstone rock formation in the southern part of the '+
+        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+        'south west of the nearest large town, Alice Springs; 450&#160;km '+
+        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+        'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+        'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+        'Aboriginal people of the area. It has many springs, waterholes, '+
+        'rock caves and ancient paintings. Uluru is listed as a World '+
+        'Heritage Site.</p>'+
+        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+        'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+        '(last visited June 22, 2009).</p>'+
+        '</div>'+
+        '</div>'
+    },
 
+    {
+        name: 'Hyde Park',
+        latLng: {lat: -33.873004, lng: 151.211405},
+        description: 'House'
+    },
 
-//map
-var googleMap = '<div id="map"></div>';
-$("#mapDiv").append(googleMap);
+    {
+        name: 'Centennial Park',
+        latLng: {lat: -33.899100, lng: 151.234040},
+        description: ''
+    },
+    {
+        name: 'Sydney Park',
+        latLng: {lat: -33.910511, lng: 151.184749},
+        description: ''
+    },
 
-/*
- Start here! initializeMap() is called when page is loaded.
- */
-function initializeMap() {
+    {
+        name: 'Chinese Garden of Friendship',
+        latLng: {lat: -33.876531, lng: 151.202768},
+        description: ''
+    },
 
-    var locations;
+    {
+        name: 'Moore Park',
+        latLng: {lat: -33.897056, lng: 151.218815},
+        description: ''
+    }
+];
 
-    var mapOptions = {
-        disableDefaultUI: true
+var map;
+
+    var place = function(locations) {
+        locations.forEach(function (location) {
+            var contentString = location.description;
+
+            var infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
+            var marker = new google.maps.Marker({
+                position: location.latLng,
+                map: map,
+                title: location.name
+            });
+
+            marker.addListener('click', function () {
+                infowindow.open(map, marker);
+            });
+
+        })
     };
 
-    // This next line makes `map` a new Google Map JavaScript Object and attaches it to
-    // <div id="map">, which is appended as part of an exercise late in the course.
-    map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+    function initMap() {
+
+        var myLatLng = {lat: -33.867853, lng: 151.215937};
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: myLatLng,
+            zoom: 8,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            styles: //styles go in here
+                [
+                    {
+                        "featureType": "road",
+                        "stylers": [
+                            {"hue": "#f600ff"},
+                            {"saturation": 2},
+                            {"lightness": 34}
+                        ]
+                    }
+                ]
+        });
 
 
-    
-    /*
-     createMapMarker(placeData) reads Google Places search results to create map pins.
-     placeData is the object returned from search results containing information
-     about a single location.
-     */
-    function createMapMarker(placeData) {
+        /*var contentString = "This is a Marker";
 
-        // The next lines save location data from the search result object to local variables
-        var lat = placeData.geometry.location.lat();  // latitude from the place service
-        var lon = placeData.geometry.location.lng();  // longitude from the place service
-        var name = placeData.formatted_address;   // name of the place from the place service
-        var bounds = window.mapBounds;            // current boundaries of the map window
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
 
-        // marker is an object with additional data about the pin for a single location
         var marker = new google.maps.Marker({
+            position: myLatLng,
             map: map,
-            position: placeData.geometry.location,
-            title: name
+            title: 'Uluru (Ayers Rock)'
         });
-
-        // infoWindows are the little helper windows that open when you click
-        // or hover over a pin on a map. They usually contain more information
-        // about a location.
-        var infoWindow = new google.maps.InfoWindow({
-            content: name
-        });
-
-        // hmmmm, I wonder what this is about...
-        google.maps.event.addListener(marker, 'click', function() {
-            infoWindow.open(map, marker);
-        });
-        // this is where the pin actually gets added to the map.
-        // bounds.extend() takes in a map location object
-        bounds.extend(new google.maps.LatLng(lat, lon));
-        // fit the map to the new marker
-        map.fitBounds(bounds);
-        // center the map
-        map.setCenter(bounds.getCenter());
+        marker.addListener('click', function () {
+            infowindow.open(map, marker);
+        });*/
+        place(locationData);
     }
 
-    /*
-     callback(results, status) makes sure the search returned results for a location.
-     If so, it creates a new map marker for that location.
-     */
-    function callback(results, status) {
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-            createMapMarker(results[0]);
-        }
-    }
-
-    /*
-     pinPoster(locations) takes in the array of locations created by locationFinder()
-     and fires off Google place searches for each location
-     */
-    function pinPoster(locations) {
-
-        // creates a Google place search service object. PlacesService does the work of
-        // actually searching for location data.
-        var service = new google.maps.places.PlacesService(map);
-
-        // Iterates through the array of locations, creates a search object for each location
-        for (var place in locations) {
-
-            // the search request object
-            var request = {
-                query: locations[place]
-            };
-
-            // Actually searches the Google Maps API for location data and runs the callback
-            // function with the search results after each search.
-            service.textSearch(request, callback);
-        }
-    }
-
-    // Sets the boundaries of the map based on pin locations
-    window.mapBounds = new google.maps.LatLngBounds();
-
-    // locations is an array of location strings returned from locationFinder()
-    locations = ["Sydney", "Wellington"];
-
-    // pinPoster(locations) creates pins on the map for each location in
-    // the locations array
-    pinPoster(locations);
-
-}
-
-/*
- Uncomment the code below when you're ready to implement a Google Map!
- */
-
-// Calls the initializeMap() function when the page loads
-window.addEventListener('load', initializeMap);
-
-// Vanilla JS way to listen for resizing of the window
-// and adjust map bounds
-window.addEventListener('resize', function(e) {
-// Make sure the map bounds get updated on page resize
-    map.fitBounds(mapBounds);
-});
