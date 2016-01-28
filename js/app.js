e9661613c63d2b1f16a07c3face70e5ed4cd7bf8
Roseanna McFarlane <play.with.fashion@gmail.com>
2016-01-28 22:39:45 +1100

    added images and description to infowindows

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
 // hard coded locations data
 var locationData = [
     {
-        name: 'Royal Botanic Gardens',
+        name: 'Royal Botanic Gardens, Sydney',
         latLng: {lat: -33.868691, lng: 151.217736},
-        description: 'html can go in here'
+        description: ''
+
     },
 
     {
-        name: 'Hyde Park',
+        name: 'Hyde Park, Sydney',
         latLng: {lat: -33.873004, lng: 151.211405},
         description: 'House'
     },
 
     {
-        name: 'Centennial Park',
+        name: 'Centennial Park, New South Wales',
         latLng: {lat: -33.899100, lng: 151.234040},
         description: ''
     },
@@ -30,11 +31,16 @@ var locationData = [
     },
 
     {
-        name: 'Moore Park',
+        name: 'Moore Park, New South Wales',
         latLng: {lat: -33.897056, lng: 151.218815},
         description: ''
     }
 ];
+
+/*
+var wikiDescriptionUrl = '//https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=Stack%20Overflow';
+var wikiImageUrl = '';
+*/
 
 function init() {//need this for map to work
 
@@ -58,33 +64,52 @@ function init() {//need this for map to work
 
     // Location constructor
     function addMarker(location) {
-        var contentString = location.description;
+        /*var contentString = location.description;*/
+        $.ajax({
+            url:'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=' + location.name + '&indexpageids=true&exsentences=1',
+            dataType: "jsonp",
+            success: function (descriptionResponse) {
+                var articleId = descriptionResponse.query.pageids[0]; //capture the id to use below
+                var descriptionArticleId = descriptionResponse.query.pages[articleId].extract;
+                console.log(descriptionArticleId);
+                $.ajax({
+                    url:'https://en.wikipedia.org/w/api.php?action=query&titles=' + location.name + '&prop=pageimages&format=json&pithumbsize=100',
+                    dataType: "jsonp",
+                    success: function (imageResponse) {
+                        var descriptionImage = imageResponse.query.pages[articleId].thumbnail.source;
+                        console.log(descriptionImage);
+                        var infowindow = new google.maps.InfoWindow({
+                            zIndex: 10,
+                            content: '<div id="markerDiv"><p>' + descriptionArticleId + '</p><img id="markerImage" src="' + descriptionImage + '"></div>'
+                        });
 
-        var infowindow = new google.maps.InfoWindow({
-            content: contentString
-        });
-        var marker = new google.maps.Marker({
-            animation: google.maps.Animation.DROP,
-            position: location.latLng,
-            map: map,
-            icon: 'images/marker.png',
-            title: location.name
-        });
+                        var marker = new google.maps.Marker({
+                            animation: google.maps.Animation.DROP,
+                            position: location.latLng,
+                            map: map,
+                            icon: 'images/marker.png',
+                            title: location.name
+                        });
 
-        marker.addListener('click', function () {
-            infowindow.open(map, marker);
-        });
+                        marker.addListener('click', function () {
+                            infowindow.open(map, marker);
+                        });
 
-        marker.addListener('click', toggleBounce);
+                        marker.addListener('click', toggleBounce);
 
-        function toggleBounce() {
-            if (marker.getAnimation() !== null) {
-                marker.setAnimation(null);
-            } else {
-                marker.setAnimation(google.maps.Animation.BOUNCE);
+                        function toggleBounce() {
+                            if (marker.getAnimation() !== null) {
+                                marker.setAnimation(null);
+                            } else {
+                                marker.setAnimation(google.maps.Animation.BOUNCE);
+                            }
+                        }
+                        markers.push(marker); //So we can delete them later.
+
+                    }
+                })
             }
-        }
-        markers.push(marker); //So we can delete them later.
+        });
     }
 
     function deleteMarkers() {
@@ -118,3 +143,42 @@ function init() {//need this for map to work
 
     ko.applyBindings(myViewModel);
 }
+
+//Api calls
+
+/*var locationNames =[];
+locationData.forEach(function(location){
+    locationNames.push(location.name)
+});
+console.log(locationNames);
+
+var placeName = $ ('#searchName').textInput;
+console.log(placeName);
+var $wikiElem = $('#content');
+
+var wikiURL = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + placeName + '&format=json&callback=wikiCallback'; //test url
+var wikiURL2 = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=' + locationData.name + '';
+console.log(wikiURL2);
+
+
+$.ajax({
+        url:wikiURL,
+        dataType: "jsonp",
+        success: function(response){
+            var articleList = response[1];
+
+            for (var i = 0; i < articleList.length; i++){
+                articleStr = articleList[i];
+                var url = 'https://en.wikipedia.org/wiki/'+ articleStr;
+                $wikiElem.append('<li><a href="' + url + '">'+
+                    articleStr + '</a></li>');
+            }
+            /!*articleList.forEach(function(articleStr){
+             var url = 'https://en.wikipedia.org/wiki/'+ articleStr;
+             $wikiElem.append('<li><a href="' + url + '">'+
+             articleStr + '</a></li>');
+             })*!/
+        }
+    }
+
+);*/
